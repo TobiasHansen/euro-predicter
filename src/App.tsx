e7540fb041ce81d499936eb3dbcdd763cb35def7
Tobias Hansen <hato1505@gmail.com>
2021-06-14 16:02:01 +0200

    Add group tables with predictions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Papa from 'papaparse';
+import {GroupTable} from "./GroupTable";
+
+const playerNames: string[] = ["yngve", "tobias", "wilberg", "ra"];
+const groups: string[] = ["Group A", "Group B", "Group C", "Group D", "Group E", "Group F"]
+
+export interface Game {
+    id: number,
+    time: string,
+    type: string,
+    homeTeam: string,
+    homeScore: number,
+    awayTeam: string,
+    awayScore: number
+}
+
+export interface Player {
+    name: string,
+    predictions: Game[]
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [players, setPlayers] = useState<Player[]>([])
+    const [results, setResults] = useState<Game[]>([])
+
+    useEffect(() => {
+        const a: Player[] = []
+        playerNames.forEach(name => {
+            Papa.parse<Game>(`./${name}.csv`, {
+                header: true,
+                download: true,
+                skipEmptyLines: true,
+                complete: data => {
+                    a.push({
+                        name,
+                        predictions: data.data.sort((a, b) => a.time > b.time ? -1 : a.time === b.time ? 0 : 1)
+                    })
+                }
+            });
+        })
+        setPlayers(a)
+        Papa.parse<Game>(`./results.csv`, {
+            header: true,
+            download: true,
+            skipEmptyLines: true,
+            complete: data => {
+                setResults(data.data)
+            }
+        });
+    }, [])
+
+    return (
+        <div style={{display: 'flex', flexWrap: 'wrap'}}>
+            {players.length > 0 && results.length > 0 && (
+                groups.map(group => <GroupTable group={group} results={results} players={players}/>)
+            )}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/GroupTable.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import {Paper} from "@material-ui/core";
+import MaterialTable from "@material-table/core";
+import {Game, Player} from "./App";
+
+export function GroupTable({group, results, players}: { group: string, results: Game[], players: Player[] }) {
+    const gamesInGroup = results.filter(game => game.type === group);
+    return (
+        <Paper style={{width: '700px', margin: '5px'}} elevation={3}>
+            <MaterialTable
+                columns={[
+                    {title: 'Tid', field: 'time', width: '60px'},
+                    {title: 'Hjemmelag', field: 'homeTeam', width: '75px'},
+                    {title: '', field: 'homeScore', width: '30px'},
+                    {title: '', field: 'awayScore', width: '30px'},
+                    {title: 'Bortelag', field: 'awayTeam', width: '75px'},
+                ]}
+                detailPanel={rowData => {
+                    const predictions: { homeScore: number; awayScore: number; name: string }[] =
+                        players.map(p => {
+                            const game = p.predictions.find(game => game.id === rowData.id)!!;
+                            return {
+                                name: p.name,
+                                homeScore: game?.homeScore,
+                                awayScore: game?.awayScore
+                            }
+                        })
+                    return (
+                        <MaterialTable
+                            style={{marginLeft: '230px', width: '310px'}}
+                            columns={[
+                                {title: 'Navn', field: 'name', width: '90px'},
+                                {title: rowData.homeTeam, field: 'homeScore', width: '29px'},
+                                {title: rowData.awayTeam, field: 'awayScore', width: '29px'},
+                            ]}
+                            data={predictions}
+                            options={{
+                                showTitle: false,
+                                paging: false,
+                                search: false,
+                                sorting: false,
+                                toolbar: false,
+                                header: false,
+                                rowStyle: rData => {
+                                    const gameNotPlayed = rowData.homeScore.toString() === '';
+                                    const correctResult = rowData.homeScore === rData.homeScore && rowData.awayScore === rData.awayScore;
+                                    const correctWinner = (rowData.homeScore > rowData.awayScore && rData.homeScore > rData.awayScore)
+                                        || (rowData.homeScore === rowData.awayScore && rData.homeScore === rData.awayScore)
+                                        || (rowData.homeScore < rowData.awayScore && rData.homeScore < rData.awayScore)
+                                    return {
+                                        backgroundColor: gameNotPlayed ? '' : correctResult ? 'green' : correctWinner ? 'orange' : 'red'
+                                    }
+                                }
+                            }}
+                        />
+                    )
+                }}
+                data={[...gamesInGroup]}
+                title={group}
+                onRowClick={(event, rowData, togglePanel) => togglePanel ? togglePanel() : {}}
+                options={{
+                    paging: false,
+                    search: false,
+                    sorting: false,
+                    header: false,
+                    tableLayout: 'fixed',
+                }}
+            />
+        </Paper>
+    )
+}
